2fbb1a7a97245309436c8f273214a9ae09a2b3b4
Basavachetan GM <basu@gorewards.io>
2026-01-08 23:26:38 +0530

    Text

--- app/component/HeroSection/HeroHeading.tsx
+++ app/component/HeroSection/HeroHeading.tsx
@@ -4,7 +4,7 @@
  */
 export default function HeroHeading() {
   return (
-    <h1 className="text-2xl md:text-4xl font-bold text-center mb-6 md:mb-8 max-w-4xl font-sans">
+    <h1 className="text-2xl md:text-4xl font-bold text-center mb-6 md:mb-8 max-w-4xl font-sans text-black">
       The native Next.js platform.
     </h1>
   );
